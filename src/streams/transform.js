19ba64804bb5b6edf46f377f9ff003ffc92772c2
mitrofazxc <mitrofanzxc@gmail.com>
2024-10-04 23:14:34 +0300

    feat: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { stdin, stdout } from "node:process";
+
+const ERROR_MESSAGE = "STREAM operation failed";
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        const reverseTransform = new Transform({
+            transform(chunk, _, callback) {
+                const reversedText = chunk?.toString()?.split("")?.reverse()?.join("");
+
+                callback(null, `${reversedText}\n`);
+            },
+        });
+
+        await pipeline(stdin, reverseTransform, stdout);
+    } catch {
+        throw new Error(ERROR_MESSAGE);
+    }
 };
 
 await transform();
