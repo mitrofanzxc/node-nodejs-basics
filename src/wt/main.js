807b3059a281ec0edace65236842782596d64009
mitrofazxc <mitrofanzxc@gmail.com>
2024-10-06 14:19:22 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { availableParallelism } from "node:os";
+import { Worker } from "node:worker_threads";
+
+const FINAL_FILENAME = "worker.js";
+const ERROR_MESSAGE = "WT operation failed";
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        const ENTRY_FILE_PATH = fileURLToPath(import.meta.url);
+        const ENTRY_DIRNAME_PATH = join(dirname(ENTRY_FILE_PATH));
+        const FINAL_FILENAME_PATH = join(ENTRY_DIRNAME_PATH, FINAL_FILENAME);
+
+        const numCores = availableParallelism();
+        const workers = [];
+
+        const createWorker = (data) =>
+            new Promise((resolve) => {
+                const worker = new Worker(FINAL_FILENAME_PATH, { workerData: data });
+
+                worker.on("message", (data) => resolve({ status: "resolved", data }));
+                worker.on("error", () => resolve({ status: "error", data: null }));
+            });
+
+        for (let i = 0; i < numCores; i++) {
+            workers.push(createWorker(i + 10));
+        }
+
+        const workerResults = await Promise.all(workers);
+
+        console.log(workerResults);
+    } catch {
+        throw new Error(ERROR_MESSAGE);
+    }
 };
 
 await performCalculations();
