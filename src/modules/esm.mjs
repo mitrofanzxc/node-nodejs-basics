81d4629d4fffff37d17bb5a3bcabfa74dd5cf00d
mitrofazxc <mitrofanzxc@gmail.com>
2024-10-06 23:43:45 +0300

    refactor: modules

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,12 +1,12 @@
 import { dirname, sep } from "node:path";
 import { fileURLToPath } from "node:url";
 import { release, version } from "node:os";
-import { createServer } from "node:http";
+import { createServer as createServerHttp } from "node:http";
 
 import "./files/c.js";
 
-const ENTRY_FILE_PATH = fileURLToPath(import.meta.url);
-const ENTRY_DIRNAME_PATH = dirname(ENTRY_FILE_PATH);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const PORT = 3000;
 const INCOMING_MESSAGE = "Request accepted";
 
@@ -20,11 +20,11 @@ export const unknownObject =
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${sep}"`);
-console.log(`Path to current file is ${ENTRY_FILE_PATH}`);
-console.log(`Path to current directory is ${ENTRY_DIRNAME_PATH}`);
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
 console.log(unknownObject);
 
-export const myServer = createServer((_, response) => {
+export const myServer = createServerHttp((_, response) => {
     response.end(INCOMING_MESSAGE);
 });
 
